8e29be896c6d6049212cdbb0c356540b963245a1
johnschaefer <jhnvinschf@ksu.edu>
2021-11-06 12:22:15 -0500

    selection fixed

--- checkboxes.js
+++ checkboxes.js
@@ -9,5 +9,5 @@ function getSelectedCheckboxValues(name) {
 
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', (event) => {
-    alert(getSelectedCheckboxValues('color'));
+    alert(getSelectedCheckboxValues('class'));
 });
