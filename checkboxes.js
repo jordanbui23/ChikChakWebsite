51572000a11a6f023b5ea413ee900a0ca1dc9f8f
jordanbui23 <jordanbui20@gmail.com>
2021-11-06 23:57:06 -0500

    Added a navbar with a developer dropdown

--- checkboxes.js
+++ checkboxes.js
@@ -7,7 +7,6 @@ function getSelectedCheckboxValues(name) {
     return values;
 }
 
-<<<<<<< HEAD
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', (event) => {
     let content = alert(getSelectedCheckboxValues('class'));
@@ -24,19 +23,3 @@ fs.writeFile('test.txt', "yo", err => {
         return;
     }
 });
-=======
-function writeToFile() {
-    const btn = document.querySelector('#btn');
-    btn.addEventListener('click', (event) => {
-        let content = alert(getSelectedCheckboxValues('class'));
-        content = getSelectedCheckboxValues('class');
-        const fs = require('fs');
-        fs.writeFile('test.txt', content, err => {
-            if (err) {
-                console.err;
-                return;
-            }
-        });
-    });
-}
->>>>>>> f64ac9d5ac322166eea2f4e3686a2f005069a4f8
